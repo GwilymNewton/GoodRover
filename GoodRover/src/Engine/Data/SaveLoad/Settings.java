9f37703a7ad7c87e8f89cc4e8a83f74c9405d432
Gwilym Newton <gwilymnewton@gmail.com>
2016-03-28 09:09:58 +0100

    First Pass of Settings System

---
+++ GoodRover/src/Engine/Data/SaveLoad/Settings.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Engine.Data.SaveLoad;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author gwilymnewton
+ */
+public class Settings {
+    //First pass, this should be coded to a write though hashmap when I have time
+    
+    private static HashMap<String,String> settings =new HashMap<String,String>() {{
+    put("inventory_size","10");
+    }};
+
+    /**
+     *
+     * @param attribute name of the attribute required
+     * @return value if found, else null
+     */
+public String GetSettingString(String attribute)
+{
+    if (settings.keySet().contains(attribute))
+    {
+        return settings.get(attribute);
+    }
+    else
+    {
+        return null;
+    }
+}
+
+public int GetSettingInt(String attribute)
+{
+    if (settings.keySet().contains(attribute))
+    {
+        return Integer.valueOf(settings.get(attribute));
+    }
+    else
+    {
+        // Not a great plan, maybe throw an exception instead?
+        return Integer.MIN_VALUE;
+    }
+}
+
+
+
+}
