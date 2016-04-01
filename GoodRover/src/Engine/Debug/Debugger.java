08d2afb6c4516763b347e0d4feab333ca3fe59dd
Gwilym Newton <gwilymnewton@gmail.com>
2016-04-01 13:14:06 +0100

    added colors to debugger

--- GoodRover/src/Engine/Debug/Debugger.java
+++ GoodRover/src/Engine/Debug/Debugger.java
@@ -13,6 +13,17 @@ import Engine.SaveLoad.Settings;
  */
 public class Debugger {
 
+    //colors for use in debuging
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     /**
      * 0= nothing 
      * 5 = everything
@@ -29,7 +40,7 @@ public class Debugger {
 {
     if (level <= debug_level)
     {
-        System.out.println("\u001B[31m"+text);
+        System.out.println(ANSI_RED+text);
     }
 }
 
